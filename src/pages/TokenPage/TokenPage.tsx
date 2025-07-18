af0c594f2936776998152ac8527874e469ad1e06
Daniel Taiakin <daniel@pewpee.com>
2025-07-18 03:41:31 +0300

    fix: chart shows mcap

--- src/pages/TokenPage/TokenPage.tsx
+++ src/pages/TokenPage/TokenPage.tsx
@@ -334,7 +334,7 @@ export const TokenPage: FC = () => {
           
           <PriceChart 
             tokenAddress={tokenAddress || ""} 
-            tokenSupply={tokenData?.token.max_supply ?? 1^6}
+            tokenSupply={tokenData?.token.max_supply ?? 1e9 }
             initialPrice={tokenData?.metrics?.price_sol ? parseFloat(tokenData.metrics.price_sol.toString()) : 0}
             height={270}
           />
